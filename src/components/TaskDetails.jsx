d700854a876f378d7ee0c84b94d67fe2beac420b
PauloAbrahao <ab.cod@outlook.com>
2023-11-16 15:11:52 -0300

    refactor: refactor app and fix some conflicts

--- src/components/TaskDetails.jsx
+++ src/components/TaskDetails.jsx
@@ -1,34 +1,31 @@
-import React from 'react';
-import { useParams, useHistory } from 'react-router-dom'
+import React from "react";
+import { useParams, useHistory } from "react-router-dom";
 
-import './TaskDetails.css'
+import "./TaskDetails.css";
 
-import Button from './Button'
+import Button from "./Button";
 
 const TaskDetails = () => {
+  const params = useParams();
 
-    const params = useParams();
+  const history = useHistory();
 
-    const history = useHistory()
+  const handleBackButtonClick = () => {
+    history.goBack();
+  };
 
-    const handleBackButtonClick= () => {
-        history.goBack();
-    }
+  return (
+    <>
+      <div className="back-button-container">
+        <Button onClick={handleBackButtonClick}>Voltar</Button>
+      </div>
 
-    return (
-        <>
-            <div className="back-button-container">
-                <Button onClick={handleBackButtonClick}>Voltar</Button>
-            </div>
-            
-            <div className="task-details-container">
+      <div className="task-details-container">
+        <h2>{params.taskTitle}</h2>
 
-                <h2>{params.taskTitle}</h2>
+      </div>
+    </>
+  );
+};
 
-                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Pariatur totam porro atque fuga suscipit ab consequatur cum aperiam libero. Dolorum labore iste repellat ad vero tempore quos aperiam possimus saepe!</p>
-            </div>
-        </>
-    );
-}
- 
 export default TaskDetails;
